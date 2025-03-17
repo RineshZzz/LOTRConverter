c76c882ebe3e2f793293ddec5bb557f3e92758da
Rinesh <rineshedge@icloud.com>
2025-03-17 08:38:54 +0530

    Added Conversion

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -8,6 +8,9 @@ struct ContentView: View {
     @State var leftAmount: String = ""
     @State var rightAmount: String = ""
     
+    @FocusState var leftTyping
+    @FocusState var rightTyping
+    
     @State var leftCurrency: Currency = .silverPiece
     @State var rightCurrency: Currency = .goldPiece
     
@@ -45,6 +48,8 @@ struct ContentView: View {
                         
                         TextField("Amount", text: $leftAmount)
                             .textFieldStyle(.roundedBorder)
+                            .focused($leftTyping)
+                            
                     }
                     Image(systemName: "equal")
                         .font(.largeTitle)
@@ -70,10 +75,13 @@ struct ContentView: View {
                         TextField("Amount", text: $rightAmount)
                             .textFieldStyle(.roundedBorder)
                             .multilineTextAlignment(.trailing)
+                            .focused($rightTyping)
+                            
                     }
                 }
                 .padding()
                 .background(.black .opacity(0.5))
+                .keyboardType(.decimalPad)
                 
                 Spacer()
                 
@@ -87,6 +95,23 @@ struct ContentView: View {
                             .foregroundStyle(.white)
                     }
                     .padding(.trailing)
+                    .onChange(of: leftAmount) {
+                        if leftTyping {
+                            rightAmount = leftCurrency.convert(leftAmount, to: rightCurrency)
+                        }
+                    }
+                    .onChange(of: rightAmount) {
+                        if rightTyping {
+                            leftAmount = rightCurrency.convert(rightAmount, to: leftCurrency)
+                        }
+                    }
+                    .onChange(of: leftCurrency) {
+                        leftAmount = rightCurrency.convert(rightAmount, to: leftCurrency)
+                    }
+                    .onChange(of: rightCurrency) {
+                        rightAmount = leftCurrency.convert(leftAmount, to: rightCurrency)
+                    }
+                    
                     .sheet(isPresented: $showExchangeInfo) {
                         ExchangeInfo()
                     }
